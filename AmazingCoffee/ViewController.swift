afc100620e29aead523066147d30d563cc2b98e4
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2021-07-13 22:16:13 +0100

    1st cut
    - added pods
    - added Table VC
    - made call to api

--- AmazingCoffee/ViewController.swift
+++ AmazingCoffee/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+
+let apiBaseURL = "https://api.foursquare.com/v2/venues/explore"
+let clientId = "14X4CLJR23QG3U53AJNYXFO5WC3SNWENWZNQFDO45CERROQ0"
+let clientSecrect = "02FWFLU4GLGT3LYPSJGAKZSXM5E0KXPYACSR0JPRWHTETLWX"
+let apiVersion = "20180323"
+let radius = "100"
+let latLong = "40.7243,-74.0018"
+let section = "coffee"
+
+struct CoffeeShopModel {
+    
+}
+
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadData(completion: { data in
+          print(data)
+        })
     }
+    
+    func loadData(completion: @escaping (CoffeeShopModel?) -> Void) {
+        
+        let completeUrl = "\(apiBaseURL)?client_id=\(clientId)&client_secret=\(clientSecrect)&v=\(apiVersion)&radius=\(radius)&ll=\(latLong)&section=\(section)"
+           
+           var CoffeeShopModel: CoffeeShopModel? = nil
+           
+           AF.request(completeUrl).responseJSON { response in
+               switch response.result {
+               case .success(let value):
+                   let json = JSON(value)
+                   print(json)
+
+                   completion(CoffeeShopModel)
+               case .failure(let error):
+                   print(error)
+                   completion(nil)
+                  
+               }
+           }
+       }}
 
 
-}
 
--- Podfile
+++ Podfile
@@ -1,21 +1,9 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+
 target 'AmazingCoffee' do
-  # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
-
-  # Pods for AmazingCoffee
-
-  target 'AmazingCoffeeTests' do
-    inherit! :search_paths
     pod 'SwiftyJSON', '~> 4.0'
     pod 'Alamofire', '~> 5.2'
-  end
-
-  target 'AmazingCoffeeUITests' do
-    pod 'SwiftyJSON', '~> 4.0'
-    pod 'Alamofire', '~> 5.2'
-  end
 
 end
